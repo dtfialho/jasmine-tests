300a6f269c0dddc4d05551a676f45ce97da9e59a
Diego T. Fialho <diego.tfialho@gmail.com>
2017-01-13 16:59:24 -0200

    Init

---
+++ app/app.js
@@ -0,0 +1,8 @@
+(function() {
+	'use strict';
+
+	angular.module('meetIrl', ['ui.router', 'api.users', 'components.users'])
+	.config(function($urlRouterProvider) {
+		$urlRouterProvider.otherwise('/');
+	});
+})();
